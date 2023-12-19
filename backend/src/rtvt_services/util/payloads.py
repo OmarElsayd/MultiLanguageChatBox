3001073deecbc5abbca2caf00a859f3b2147a694
omarelsayd <omar_2546@hotmail.com>
2023-12-19 16:54:01 -0500

    RTVT-4-session and payloads

--- backend/src/rtvt_services/util/payloads.py
+++ backend/src/rtvt_services/util/payloads.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pydantic import BaseModel
 
 
 @dataclass
@@ -11,3 +12,21 @@ class InCompingTokenPayload:
 class TokenPayload:
     sub: str
     exp: float
+
+
+class UserInput(BaseModel):
+    user_name: str
+    email: str
+    password: str
+    first_name: str
+    last_name: str
+
+
+class SignupOutput(BaseModel):
+    status_code: int
+
+
+class TokenResponse(BaseModel):
+    status_code: int
+    access_token: str
+    token_type: str = "bearer"
