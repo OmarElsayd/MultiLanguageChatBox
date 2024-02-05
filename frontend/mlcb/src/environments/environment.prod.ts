3de2cdc3d520779ad09f2e5a45b91e978d642b18
OmarElsayd <94146702+OmarElsayd@users.noreply.github.com>
2024-02-04 23:53:40 -0500

    Update environment.prod.ts

--- frontend/mlcb/src/environments/environment.prod.ts
+++ frontend/mlcb/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_ROUTE: 'mlcb/api/v1',
-  BASE_DOMAIN_ROUTE: 'multilanguagechatboxbackend-qqahzd6gxa-uc.a.run.app:5678'
+  BASE_DOMAIN_ROUTE: 'multilanguagechatboxbackend-qqahzd6gxa-uc.a.run.app'
 };
